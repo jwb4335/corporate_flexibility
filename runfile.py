fb5fa154bec0f88f46108b529e7162113f1f3579
John W Barry <51172981+jwb4335@users.noreply.github.com>
2022-01-31 13:40:40 -0500

    Add files via upload

--- runfile.py
+++ runfile.py
@@ -34,8 +34,7 @@ print("Building workplace flexibility measure at 4-digit NAICS...",end = '\n\n')
 workplace_flex = build_workplace_flex()
 
 print("""Building investment flexibility measure at 4-digit NAICS...
-(note: raw data not included here as it is proprietary survey data)
-""",end = '\n\n')
+(note: raw data not included here as it is proprietary survey data, so function will just exit)""",end = '\n\n')
 investment_flex = build_investment_flex()
 
 print("Building customer interactions measure using direct and indirect customer interactions at 4-digit NAICS...",end = '\n\n')
@@ -46,8 +45,8 @@ fixed_cost_share = build_fixed_cost_share()
 
 ## Save data
 workplace_flex.to_csv("Out_data/workplace_flex.csv",index=False)
-investment_flex.to_csv("Out_data/investment_flex.csv",index=False)
 cust_int.to_csv("Out_data/cust_int.csv",index=False)
 fixed_cost_share.to_csv("Out_data/fixed_cost_share.csv",index=False)
+# investment_flex.to_csv("Out_data/investment_flex.csv",index=False)
 
 print("Done!")
